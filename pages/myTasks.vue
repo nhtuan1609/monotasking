035f974cfb0443974aaad81aafb1c67e500debeb
nhtuan1609 <nguyenhoangtuanth98@gmail.com>
2021-10-19 11:50:29 +0700

    update initial structure for pages

---
+++ pages/myTasks.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout fill-height>
+    <v-flex>
+      <v-tabs v-model="tab">
+        <v-tab>List</v-tab>
+        <v-tab>Board</v-tab>
+        <v-tab>Calander</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab" class="ma-2">
+        <v-tab-item>
+          <span>List</span>
+        </v-tab-item>
+        <v-tab-item>
+          <span>Board</span>
+        </v-tab-item>
+        <v-tab-item>
+          <span>Calander</span>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: 0
+    }
+  }
+}
+</script>
