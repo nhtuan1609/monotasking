52caa714915fd250acdf6fcbc43a8a3c76e14784
tuan.n <nhtuan1609@gmail.com>
2021-12-13 15:57:11 +0700

    [create] ContextMenu component for task list

---
+++ components/myTasks/list/ContextMenu.vue
@@ -0,0 +1,295 @@
+<template>
+  <v-menu
+    v-model="isShowContextMenuTemp"
+    min-width="200"
+    :position-x="menuX"
+    :position-y="menuY"
+    absolute
+    transition="scale-transition"
+    offset-y
+    z-index="9"
+  >
+    <v-list light dense>
+      <!-- change status -->
+      <v-menu :key="'status-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-circle-slice-4</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Status</v-list-item-title>
+            <v-icon small>mdi-menu-right</v-icon>
+          </v-list-item>
+        </template>
+        <status-select-menu
+          :statuses="statuses"
+          :task="selectedTask"
+          @selected="$parent.isShowContextMenu = false"
+        ></status-select-menu>
+      </v-menu>
+
+      <!-- change assignee -->
+      <v-menu :key="'assignee-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-account-arrow-right</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Assignee</v-list-item-title>
+            <v-icon small>mdi-menu-right</v-icon>
+          </v-list-item>
+        </template>
+        <assignee-select-menu
+          :members="members"
+          :task="selectedTask"
+          @selected="$parent.isShowContextMenu = false"
+        ></assignee-select-menu>
+      </v-menu>
+
+      <!-- change priority -->
+      <v-menu :key="'priority-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-signal-cellular-3</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Priority</v-list-item-title>
+            <v-icon small>mdi-menu-right</v-icon>
+          </v-list-item>
+        </template>
+        <priority-select-menu
+          :priorities="priorities"
+          :task="selectedTask"
+          @selected="$parent.isShowContextMenu = false"
+        ></priority-select-menu>
+      </v-menu>
+
+      <!-- change label -->
+      <v-menu :key="'label-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-label</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Labels</v-list-item-title>
+            <v-icon small>mdi-menu-right</v-icon>
+          </v-list-item>
+        </template>
+        <label-select-menu
+          :labels="labels"
+          :task="selectedTask"
+          @selected="$parent.isShowContextMenu = false"
+        ></label-select-menu>
+      </v-menu>
+
+      <!-- change project -->
+      <v-menu :key="'project-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-view-grid-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Projects</v-list-item-title>
+            <v-icon small>mdi-menu-right</v-icon>
+          </v-list-item>
+        </template>
+        <project-select-menu
+          :projects="projects"
+          :task="selectedTask"
+          @selected="$parent.isShowContextMenu = false"
+        ></project-select-menu>
+      </v-menu>
+
+      <!-- change due date -->
+      <v-dialog v-model="datePickerDueDate" max-width="290px">
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-calendar</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Change due date...</v-list-item-title>
+          </v-list-item>
+        </template>
+        <v-date-picker
+          color="primary"
+          light
+          no-title
+          :value="selectedTask.dueDate"
+          @input="datePickerDueDate = false"
+          @change="changeDueDate"
+        >
+          <v-spacer></v-spacer>
+          <v-btn text @click="changeDueDate()">Clear</v-btn>
+        </v-date-picker>
+      </v-dialog>
+
+      <!-- rename -->
+      <v-dialog v-model="isShowRenameDialog" width="500">
+        <template #activator="{ on, attrs }">
+          <v-list-item v-bind="attrs" v-on="on">
+            <v-list-item-icon class="mr-2">
+              <v-icon small>mdi-pencil</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Rename...</v-list-item-title>
+          </v-list-item>
+        </template>
+        <v-card light class="pa-0">
+          <v-card-title class="primary white--text py-2 px-4">Change task name</v-card-title>
+          <v-card-text class="pa-4">
+            <v-form ref="form">
+              <h4>New name</h4>
+              <v-text-field v-model="newTaskName" outlined dense autofocus :rules="[$rules.required]"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="pa-4">
+            <v-spacer></v-spacer>
+            <v-btn text outlined plain @click="cancelRename">Cancel</v-btn>
+            <v-btn elevation="0" color="primary" @click="changeName">Confirm</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <!-- delete task -->
+      <v-divider class="my-2"></v-divider>
+      <v-list-item @click="deleteTask">
+        <v-list-item-icon class="mr-2">
+          <v-icon small>mdi-trash-can</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Delete</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+import { TASK } from '~/constants/task.js'
+import StatusSelectMenu from '~/components/common/StatusSelectMenu.vue'
+import PrioritySelectMenu from '~/components/common/PrioritySelectMenu.vue'
+import ProjectSelectMenu from '~/components/common/ProjectSelectMenu.vue'
+import AssigneeSelectMenu from '~/components/common/AssigneeSelectMenu.vue'
+import LabelSelectMenu from '~/components/common/LabelSelectMenu.vue'
+
+export default {
+  name: 'ContextMenu',
+  components: {
+    StatusSelectMenu,
+    PrioritySelectMenu,
+    ProjectSelectMenu,
+    AssigneeSelectMenu,
+    LabelSelectMenu
+  },
+  props: {
+    isShowContextMenu: {
+      type: Boolean,
+      required: true
+    },
+    selectedTask: {
+      type: Object,
+      required: true
+    },
+    menuX: {
+      type: Number,
+      default: 0
+    },
+    menuY: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      isShowContextMenuTemp: false,
+      datePickerDueDate: false,
+      isShowRenameDialog: false,
+      newTaskName: ''
+    }
+  },
+  computed: {
+    priorities() {
+      return Object.values(TASK.PRIORITY)
+    },
+    statuses() {
+      return Object.values(TASK.STATUS)
+    },
+    projects() {
+      return this.$store.getters['projects/getProjects']
+    },
+    members() {
+      return this.$store.getters['members/getMembers']
+    },
+    labels() {
+      return this.$store.getters['labels/getLabels']
+    }
+  },
+  watch: {
+    isShowRenameDialog(newValue) {
+      if (newValue) {
+        this.newTaskName = this.selectedTask.name
+      }
+    },
+    isShowContextMenu(newValue) {
+      this.isShowContextMenuTemp = newValue
+    },
+    isShowContextMenuTemp(newValue) {
+      if (!newValue) {
+        this.$parent.isShowContextMenu = newValue
+      }
+    }
+  },
+  methods: {
+    /**
+     * produce change due date for selected task
+     * @param {object} task - due date information which is selected
+     * @return {void}
+     */
+    changeDueDate(dueDate = '') {
+      if (dueDate === this.selectedTask.dueDate) return
+
+      this.$store.dispatch('tasks/updateTask', {
+        id: this.selectedTask.id,
+        data: { dueDate },
+        activityType: TASK.ACTIVITY_TYPE.CHANGE_DUE_DATE
+      })
+      this.$parent.isShowContextMenu = false
+      this.datePickerDueDate = false
+    },
+    /**
+     * produce delete selected task
+     * @return {void}
+     */
+    deleteTask() {
+      this.$store.dispatch('tasks/deleteTask', { id: this.selectedTask.id })
+      this.$parent.isShowContextMenu = false
+    },
+    /**
+     * close rename dialog nad context menu
+     * @return {void}
+     */
+    cancelRename() {
+      this.isShowRenameDialog = false
+      this.$parent.isShowContextMenu = false
+    },
+    /**
+     * produce change name of selected task
+     * @return {void}
+     */
+    changeName() {
+      if (!this.$refs.form.validate()) return
+
+      const validatedName = this.newTaskName.trim()
+      if (validatedName && validatedName !== this.selectedTask.name) {
+        this.$store.dispatch('tasks/updateTask', {
+          id: this.selectedTask.id,
+          data: { name: validatedName },
+          activityType: TASK.ACTIVITY_TYPE.CHANGE_NAME
+        })
+      }
+
+      this.isShowRenameDialog = false
+      this.$parent.isShowContextMenu = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- components/myTasks/list/TaskList.vue
+++ components/myTasks/list/TaskList.vue
@@ -15,189 +15,26 @@
     </div>
 
     <!-- show context menu when right clicking on a task-->
-    <v-menu
-      v-model="isShowContextMenu"
-      min-width="200"
-      :position-x="menuX"
-      :position-y="menuY"
-      absolute
-      transition="scale-transition"
-      offset-y
-      z-index="9"
-    >
-      <v-list light dense>
-        <!-- change status -->
-        <v-menu :key="'status-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-circle-slice-4</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Status</v-list-item-title>
-              <v-icon small>mdi-menu-right</v-icon>
-            </v-list-item>
-          </template>
-          <status-select-menu
-            :statuses="statuses"
-            :task="selectedTask"
-            @selected="isShowContextMenu = false"
-          ></status-select-menu>
-        </v-menu>
-
-        <!-- change assignee -->
-        <v-menu :key="'assignee-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-account-arrow-right</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Assignee</v-list-item-title>
-              <v-icon small>mdi-menu-right</v-icon>
-            </v-list-item>
-          </template>
-          <assignee-select-menu
-            :members="members"
-            :task="selectedTask"
-            @selected="isShowContextMenu = false"
-          ></assignee-select-menu>
-        </v-menu>
-
-        <!-- change priority -->
-        <v-menu :key="'priority-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-signal-cellular-3</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Priority</v-list-item-title>
-              <v-icon small>mdi-menu-right</v-icon>
-            </v-list-item>
-          </template>
-          <priority-select-menu
-            :priorities="priorities"
-            :task="selectedTask"
-            @selected="isShowContextMenu = false"
-          ></priority-select-menu>
-        </v-menu>
-
-        <!-- change label -->
-        <v-menu :key="'label-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-label</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Labels</v-list-item-title>
-              <v-icon small>mdi-menu-right</v-icon>
-            </v-list-item>
-          </template>
-          <label-select-menu
-            :labels="labels"
-            :task="selectedTask"
-            @selected="isShowContextMenu = false"
-          ></label-select-menu>
-        </v-menu>
-
-        <!-- change project -->
-        <v-menu :key="'project-' + selectedTask.id" min-width="100" offset-x nudge-top="8" open-on-hover>
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-view-grid-outline</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Projects</v-list-item-title>
-              <v-icon small>mdi-menu-right</v-icon>
-            </v-list-item>
-          </template>
-          <project-select-menu
-            :projects="projects"
-            :task="selectedTask"
-            @selected="isShowContextMenu = false"
-          ></project-select-menu>
-        </v-menu>
-
-        <!-- change due date -->
-        <v-dialog v-model="datePickerDueDate" max-width="290px">
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-calendar</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Change due date...</v-list-item-title>
-            </v-list-item>
-          </template>
-          <v-date-picker
-            color="primary"
-            light
-            no-title
-            :value="selectedTask.dueDate"
-            @input="datePickerDueDate = false"
-            @change="changeDueDate"
-          >
-            <v-spacer></v-spacer>
-            <v-btn text @click="changeDueDate()">Clear</v-btn>
-          </v-date-picker>
-        </v-dialog>
-
-        <!-- rename -->
-        <v-dialog v-model="isShowRenameDialog" width="500">
-          <template #activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
-              <v-list-item-icon class="mr-2">
-                <v-icon small>mdi-pencil</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Rename...</v-list-item-title>
-            </v-list-item>
-          </template>
-          <v-card light class="pa-0">
-            <v-card-title class="primary white--text py-2 px-4">Change task name</v-card-title>
-            <v-card-text class="pa-4">
-              <v-form ref="form">
-                <h4>New name</h4>
-                <v-text-field v-model="newTaskName" outlined dense autofocus :rules="[$rules.required]"></v-text-field>
-              </v-form>
-            </v-card-text>
-            <v-card-actions class="pa-4">
-              <v-spacer></v-spacer>
-              <v-btn text outlined plain @click="cancelRename">Cancel</v-btn>
-              <v-btn elevation="0" color="primary" @click="changeName">Confirm</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-
-        <!-- delete task -->
-        <v-divider class="my-2"></v-divider>
-        <v-list-item @click="deleteTask">
-          <v-list-item-icon class="mr-2">
-            <v-icon small>mdi-trash-can</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Delete</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <context-menu
+      :is-show-context-menu="isShowContextMenu"
+      :menu-x="menuX"
+      :menu-y="menuY"
+      :selected-task="selectedTask"
+    ></context-menu>
   </v-container>
 </template>
 
 <script>
 import WonderingCard from '~/components/myTasks/list/WonderingCard.vue'
 import TaskItem from '~/components/myTasks/list/TaskItem.vue'
-import { TASK } from '~/constants/task.js'
-import StatusSelectMenu from '~/components/common/StatusSelectMenu.vue'
-import PrioritySelectMenu from '~/components/common/PrioritySelectMenu.vue'
-import ProjectSelectMenu from '~/components/common/ProjectSelectMenu.vue'
-import AssigneeSelectMenu from '~/components/common/AssigneeSelectMenu.vue'
-import LabelSelectMenu from '~/components/common/LabelSelectMenu.vue'
+import ContextMenu from '~/components/myTasks/list/ContextMenu.vue'
 
 export default {
   name: 'TaskList',
   components: {
     WonderingCard,
     TaskItem,
-    StatusSelectMenu,
-    PrioritySelectMenu,
-    ProjectSelectMenu,
-    AssigneeSelectMenu,
-    LabelSelectMenu
+    ContextMenu
   },
   data() {
     return {
@@ -205,38 +42,15 @@ export default {
       menuX: 0,
       menuY: 0,
       selectedTask: {},
-      datePickerDueDate: false,
-      isShowRenameDialog: false,
-      newTaskName: '',
       isLoading: true
     }
   },
   computed: {
     tasks() {
       return this.$store.getters['tasks/getTasks']
-    },
-    priorities() {
-      return Object.values(TASK.PRIORITY)
-    },
-    statuses() {
-      return Object.values(TASK.STATUS)
-    },
-    projects() {
-      return this.$store.getters['projects/getProjects']
-    },
-    members() {
-      return this.$store.getters['members/getMembers']
-    },
-    labels() {
-      return this.$store.getters['labels/getLabels']
     }
   },
   watch: {
-    isShowRenameDialog(newValue) {
-      if (newValue) {
-        this.newTaskName = this.selectedTask.name
-      }
-    },
     tasks() {
       this.isLoading = false
     }
@@ -257,57 +71,6 @@ export default {
       this.menuX = event.clientX
       this.menuY = event.clientY
       this.isShowContextMenu = true
-    },
-    /**
-     * produce change due date for selected task
-     * @param {object} task - due date information which is selected
-     * @return {void}
-     */
-    changeDueDate(dueDate = '') {
-      if (dueDate === this.selectedTask.dueDate) return
-
-      this.$store.dispatch('tasks/updateTask', {
-        id: this.selectedTask.id,
-        data: { dueDate },
-        activityType: TASK.ACTIVITY_TYPE.CHANGE_DUE_DATE
-      })
-      this.isShowContextMenu = false
-      this.datePickerDueDate = false
-    },
-    /**
-     * produce delete selected task
-     * @return {void}
-     */
-    deleteTask() {
-      this.$store.dispatch('tasks/deleteTask', { id: this.selectedTask.id })
-      this.isShowContextMenu = false
-    },
-    /**
-     * close rename dialog nad context menu
-     * @return {void}
-     */
-    cancelRename() {
-      this.isShowRenameDialog = false
-      this.isShowContextMenu = false
-    },
-    /**
-     * produce change name of selected task
-     * @return {void}
-     */
-    changeName() {
-      if (!this.$refs.form.validate()) return
-
-      const validatedName = this.newTaskName.trim()
-      if (validatedName && validatedName !== this.selectedTask.name) {
-        this.$store.dispatch('tasks/updateTask', {
-          id: this.selectedTask.id,
-          data: { name: validatedName },
-          activityType: TASK.ACTIVITY_TYPE.CHANGE_NAME
-        })
-      }
-
-      this.isShowRenameDialog = false
-      this.isShowContextMenu = false
     }
   }
 }
@@ -321,14 +84,6 @@ export default {
   }
   .no-selecting {
     background-color: var(--v-primary-lighten5);
-  }
-}
-
-.task-content-chip {
-  text-transform: unset;
-  justify-content: flex-start;
-  &::before {
-    background-color: transparent !important;
   }
 }
 </style>
